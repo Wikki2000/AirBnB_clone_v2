7e143edc1eb176a5c9750cdb03c4db2fd552b746
wikki200 <wisdomokposin@gmail.com>
2024-05-01 14:54:17 -0700

    Add files

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,9 +1,6 @@
--- Create database if not exist
+-- Create test database and user
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-
--- Create user if not exist and set password
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-
--- Grant privileges on database to user
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
+GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
--- test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-from models import storage
-
-val = storage.all()
-for key, obj in val.copy().items():
-    print(f"An object: {obj} with key: {key} is about to be deleted")
-    storage.delete(obj)
